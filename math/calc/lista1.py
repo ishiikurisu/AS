24eb5b86690911625bc11952ec9ef5be4b882a38
Cris Silva Jr <ishiikurisu@yahoo.com>
2016-09-04 14:53:00 -0300

    Too lazy to write these calculations right now

---
+++ math/calc/lista1.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pylab as mpl
+
+def q1():
+    return 0
+
+def q2():
+    return 0
+
+def q3():
+    return 0
+
+if __name__ == '__main__':
+    pass
---
+++ math/calc/template.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pylab as mpl
+
+def func(x):
+    return x
+
+if __name__ == '__main__':
+    func(5)
